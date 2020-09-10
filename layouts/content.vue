be73ea501dff34877774aaceed28df126317dd38
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-09-10 10:05:46 +0900

    add:記事ページに戻るボタンを追加

--- layouts/content.vue
+++ layouts/content.vue
@@ -2,6 +2,9 @@
   <div>
     <page-header />
     <div class="container content-container">
+      <div class="home-button">
+        <back-button />
+      </div>
       <Nuxt />
       <ad-card />
     </div>
@@ -11,10 +14,12 @@
 <script>
 import pageHeader from '@/layouts/pageHeader'
 import adCard from '@/components/molecules/adCard'
+import backButton from '@/components/atoms/backButton'
 export default {
   components: {
     pageHeader,
     adCard,
+    backButton,
   },
 }
 </script>
@@ -26,5 +31,10 @@ export default {
   justify-content: space-between;
   width: 100%;
   position: relative;
+}
+.home-button {
+  position: fixed;
+  left: calc(((100vw - 1100px) / 2));
+  transform: translateX(calc(-100% - 3rem));
 }
 </style>
