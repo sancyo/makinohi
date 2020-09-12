a8dafe5d7f3ba88011be5f0a4bb40e5c5d31b51c
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-09-12 16:51:11 +0900

    fix:ファイル名の表記ミスを修正

---
+++ components/templates/postCardListSetter.vue
@@ -0,0 +1,31 @@
+<template>
+  <post-card-list :post-content="sortPostContentJson()" />
+</template>
+
+<script>
+import postContentJson from '@/content/json/content.json'
+import postCardList from '@/components/organisms/postCardList'
+export default {
+  components: {
+    postCardList,
+  },
+  data() {
+    return {
+      postContentJson,
+    }
+  },
+  methods: {
+    // 現在表示しているページに応じてJSONを整形して返すメソッド
+    sortPostContentJson() {
+      const routePath = this.$route.path
+      if (routePath === '/') {
+        return postContentJson.content
+      } else {
+        return postContentJson.content.filter(
+          (items) => items.tag === routePath.replace('/', '')
+        )
+      }
+    },
+  },
+}
+</script>
